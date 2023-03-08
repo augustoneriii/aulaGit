487342236f9890ab76f3681fd0911d3b05d0f468
Augusto Neri <augustodeo.neri@gmail.com>
2023-03-08 12:34:06 -0400

    add atualizações

--- js/calculaImc.js
+++ js/calculaImc.js
@@ -18,18 +18,35 @@ for (var i = 0; i <= pacientes.length; i++) {
     let peso = pesoTd.textContent
     // console.log(peso)
 
-
-
     //recebendo a altura do paciente
     let alturaTd = paciente.querySelector('.info-altura')
     // console.log(alturaTd)
     let altura = alturaTd.textContent
 
-
     let imcTd = paciente.querySelector('.info-imc')
     //toFixed para ajustar as casas decimais 
-    let imc = calculaImc(peso, altura)
-    imcTd.textContent = imc
+
+    let pesoEhValido = validaPeso(peso)
+    let alturaEhValido = validaAltura(altura)
+
+    if(!pesoEhValido){
+        console.log('Peso Invalido')
+        pesoEhValido = false
+        imcTd.textContent = 'Peso Invalido'
+        paciente.classList.add('paciente-invalido')
+    }
+
+    if(!alturaEhValido){
+        console.log('Altura invalida')
+        alturaEhValido = false
+        imcTd.textContent = 'Altura Invalida'
+        paciente.classList.add = 'paciente-invalido'
+    }
+
+    if(pesoEhValido && alturaEhValido){
+        let imc = calculaImc(peso, altura)
+        imcTd.textContent = imc
+    }
 }
 
 function calculaImc(peso, altura) {
@@ -37,4 +54,20 @@ function calculaImc(peso, altura) {
     //calculando o imc do paciente
     imc = peso / (altura * altura)
     return imc.toFixed(2)
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso <= 1000) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validaAltura(altura){
+    if (altura >= 0 && altura <= 3.00){
+        return true
+    } else {
+        return false
+    }
 }
--- js/form.js
+++ js/form.js
@@ -7,18 +7,63 @@ botaoAdicionar.addEventListener('click', function (evento) {
 
     let paciente = obterValoresDoForm(form)
 
+    let erros = validaPaciente(paciente)
+
+    if (erros.length > 0) {
+        exibeMensagemDeErro(erros)
+        return
+    }
+
     adicionaPacienteNaTabela(paciente)
 
+    let mensagemErro = document.querySelector("#mensagens-erro")
+    mensagemErro.innerHTML = ''
 })
 
-function adicionaPacienteNaTabela(paciente){
+function validaPaciente(paciente) {
+    let erros = []
+
+    if (paciente.nome.length == 0) {
+        erros.push('O nome não pode estar em branco')
+    }
+    if (paciente.gordura.length == 0) {
+        erros.push('O gordura não pode estar em branco')
+    }
+    if (paciente.peso.length == 0) {
+        erros.push('O peso não pode estar em branco')
+    }
+    if (paciente.altura.length == 0) {
+        erros.push('A altura não pode estar em branco')
+    }
+    if (!validaPeso(paciente.peso)) {
+        erros.push('Peso Invalido')
+    }
+    if (!validaAltura(paciente.altura)) {
+        erros.push('Altura invalida')
+    }
+
+    return erros
+}
+
+function exibeMensagemDeErro(erros) {
+    let ul = document.querySelector('#mensagens-erro')
+    ul.innerHTML = ''
+
+    erros.forEach(function (erro) {
+        let li = document.createElement('li')
+        li.textContent = erro
+        ul.appendChild(li)
+    })
+}
+
+function adicionaPacienteNaTabela(paciente) {
     let pacienteTr = montarTr(paciente)
     let tabela = document.querySelector('#tabela-pacientes')
 
     tabela.appendChild(pacienteTr)
 }
 
-function montarTr(paciente){
+function montarTr(paciente) {
     let pacienteTr = document.createElement('tr')
     pacienteTr.classList.add('paciente')
 
@@ -31,7 +76,7 @@ function montarTr(paciente){
     return pacienteTr
 }
 
-function montarTd(dado, classe){
+function montarTd(dado, classe) {
     //criando um elemeto td
     let td = document.createElement('td')
     td.textContent = dado
@@ -41,7 +86,7 @@ function montarTd(dado, classe){
     return td
 }
 
-function obterValoresDoForm(form){
+function obterValoresDoForm(form) {
     let paciente = {
         nome: form.nome.value,
         peso: form.peso.value,
